65fce0bfc1ceca981381d48ee980fa650a1db86a
rajmahendra <rajmahendra@gmail.com>
2013-11-12 17:51:50 +0530

    initial commit

---
+++ plugins/src/main/java/org/jbake/plugin/archive/ArchivePlugin.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.jbake.plugin.archive;
+
+import org.jbake.api.plugin.AbstractJBakePlugin;
+
+/**
+ *
+ * @author Rajmahendra Hegde <rajmahendra@gmail.com>
+ */
+public class ArchivePlugin extends AbstractJBakePlugin  {
+
+    @Override
+    public String getPluginName() {
+        return "Archive Plugin";
+    }
+
+    @Override
+    public void execute() {
+        
+    }
+    
+}
---
+++ plugins/src/main/java/org/jbake/plugin/feed/FeedPlugin.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.jbake.plugin.feed;
+
+import org.jbake.api.plugin.AbstractJBakePlugin;
+
+/**
+ *
+ * @author Rajmahendra Hegde <rajmahendra@gmail.com>
+ */
+public class FeedPlugin extends AbstractJBakePlugin {
+
+    @Override
+    public String getPluginName() {
+        return "Feed Plugin";
+    }
+
+    @Override
+    public void execute() {
+       
+    }
+    
+}
